23c1db9cf37ba7788ba825c59e8772fc7b112b78
Mih4s <miha.skalic@gmail.com>
2019-04-01 16:33:06 +0200

    Modified setup.

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mlagents',
+    version='0.7.0',
+    description='Unity Machine Learning Agents',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Unity-Technologies/ml-agents',
+    author='Unity Technologies',
+    author_email='ML-Agents@unity3d.com',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    packages=find_packages(exclude=['tests', 'tests.*', '*.tests', '*.tests.*']),  # Required
+
+    install_requires=[
+        'tensorflow',
+        'Pillow>=4.2.1',
+        'matplotlib',
+        'numpy',
+        'jupyter',
+        'pytest>=3.2.2,<4.0.0',
+        'docopt',
+        'pyyaml',
+        'protobuf',
+        'grpcio',
+        'pypiwin32==223;platform_system=="Windows"'],
+
+    python_requires=">=3.6,<3.7",
+
+    entry_points={
+        'console_scripts': [
+            'mlagents-learn=mlagents.trainers.learn:main',
+        ],
+    },
+)
